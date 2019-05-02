8a2eee6cd9d0dfb923db16b9c7d8c42f31d3cbb8
Yuki Okushi <huyuumi.dev@gmail.com>
2019-05-02 16:43:31 +0900

    test commit

--- server/lib/awsSdk.js
+++ server/lib/awsSdk.js
@@ -1,15 +1,14 @@
 import aws from 'aws-sdk';
+
 // import config from 'config';
 
 // s3 bucket
 let s3;
-if (config.aws.s3.key) {
-  s3 = new aws.Config({
-    accessKeyId: 'foobar',
-    secretAccessKey: 'foobar',
-    region: 'us-west-1',
-    s3BucketEndpoint: 'foobar',
-  });
-}
+s3 = new aws.Config({
+  accessKeyId: 'foobar',
+  secretAccessKey: 'foobar',
+  region: 'us-west-1',
+  s3BucketEndpoint: 'foobar',
+});
 
 export default s3;
