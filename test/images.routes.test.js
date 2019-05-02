fb30b7b46b264ed36a42faa93410b7e7409901aa
Yuki Okushi <huyuumi.dev@gmail.com>
2019-05-03 01:17:40 +0900

    test commit

--- test/images.routes.test.js
+++ test/images.routes.test.js
@@ -24,39 +24,39 @@ describe('images.routes.test.js', function() {
 
   beforeEach(() => models.User.create(userData).tap(u => (user = u)));
 
-  it('should upload an image to S3', async done => {
+  it('should upload an image to S3', done => {
     const originalImage = fs.readFileSync(path.join(__dirname, 'mocks/images/camera.png'), {
       encoding: 'utf8',
     });
-    try {
-      const res = await request(app)
-        .post(`/images/?api_key=${application.api_key}`)
-        .attach('file', 'test/mocks/images/camera.png')
-        .set('Authorization', `Bearer ${user.jwt()}`)
-        .expect(200);
-      expect(res.body.url).to.contain('.png');
-      const res_1 = await fetch(res.body.url);
-      const image = await res_1.text();
-      expect(image).to.equal(originalImage);
-      done();
-    } catch (err) {
-      return done(err);
-    }
+    request(app)
+      .post(`/images/?api_key=${application.api_key}`)
+      .attach('file', 'test/mocks/images/camera.png')
+      .set('Authorization', `Bearer ${user.jwt()}`)
+      .expect(200)
+      .then(res => {
+        expect(res.body.url).to.contain('.png');
+        return fetch(res.body.url).then(res => res.text());
+      })
+      .then(image => {
+        expect(image).to.equal(originalImage);
+        done();
+      })
+      .finally(done);
   });
 
   it('should throw an error if no file field is sent', done => {
-    return request(app)
+    request(app)
       .post(`/images/?api_key=${application.api_key}`)
       .set('Authorization', `Bearer ${user.jwt()}`)
       .expect(400)
-      .end(done);
+      .finally(done);
   });
 
   it('should upload if the user is not logged in', done => {
-    return request(app)
+    request(app)
       .post(`/images/?api_key=${application.api_key}`)
       .attach('file', 'test/mocks/images/camera.png')
       .expect(200)
-      .end(done);
+      .finally(done);
   });
 });
